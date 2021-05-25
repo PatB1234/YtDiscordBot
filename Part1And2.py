839937c985ebdef6633036885ccf398db2752962
PatB1234 <80209204+PatB1234@users.noreply.github.com>
2021-05-25 10:19:39 +0300

    Update Part1And2.py

--- Part1And2.py
+++ Part1And2.py
@@ -9,6 +9,6 @@ async def on_message(message):
     print(message.content)
     if (message.content == "Hello Bot"):
         await message.channel.send("Hello YT!")
-        await message.channel.send(file=discord.File("Website/DIY.jpg"))
+        await message.channel.send(file=discord.File("Your File Directory"))
         
 bot.run('Your Token Here')
